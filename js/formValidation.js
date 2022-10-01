ea095d0593eac83569f60a6dc398495159d5c8b0
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-10-01 09:52:27 +0200

    Create dom elements
    Ajout des éléments dom dans le fichier formValidation.js

---
+++ js/formValidation.js
@@ -0,0 +1,13 @@
+//dom elements
+const firstName = document.getElementById('first');
+const lastName = document.getElementById('last');
+const email = document.getElementById('email');
+const birthdate = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+const allLocations = document.getElementById('allLocations');
+const locations = document.querySelectorAll('#allLocations .checkbox-input');
+const checkbox1 = document.getElementById('checkbox1');
+const input = document.getElementsByClassName('text-control');
+const form = document.getElementById('form');
+const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
+//fin de dom elements
