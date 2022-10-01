896af99d5858305d7ecf106b22b4916a8766a66d
Ivaylo Rachev <rachev.ivaylo@yahoo.fr>
2022-10-01 10:05:29 +0200

    Update formValidation.js
    Ajout de vérification de nom et prénom

--- js/formValidation.js
+++ js/formValidation.js
@@ -11,3 +11,29 @@ const input = document.getElementsByClassName('text-control');
 const form = document.getElementById('form');
 const regex = /^[a-zA-ZÀ-ÖØ-öø-ÿ]+$/;
 //fin de dom elements
+
+//form validation
+
+// check names
+function checkFirstName() {
+    if (firstName.value.trim().length < 2 || first.value.trim() === '' || !firstName.value.match(regex)) {
+        firstName.parentElement.setAttribute('data-error-visible', 'true');
+        firstName.style.border = '2px solid #e54858';
+        return false;
+    }
+    first.parentElement.setAttribute('data-error-visible', 'false');
+    first.style.border = '2px #279e7a 0.19rem';
+    return true;
+}
+
+function checkLastName() {
+    if (lastName.value.trim().length < 2 || last.value.trim() === '' || !lastName.value.match(regex)) {
+        lastName.parentElement.setAttribute('data-error-visible', 'true');
+        lastName.style.border = '2px solid #e54858';
+        return false;
+    }
+    last.parentElement.setAttribute('data-error-visible', 'false');
+    last.style.border = '2px #279e7a 0.19rem';
+    return true;
+}
+//fin de check names
